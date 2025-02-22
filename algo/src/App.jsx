a19716086419d4bc726930bbad90a6ef3294cdee
Pankaj Yadav <yadavpankaj.tech@gmail.com>
2025-02-22 16:09:16 +0000

    start the project with the basic dependency

---
+++ algo/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button>Click me</button>
+    </div>
+  )
+}
+
+export default App
